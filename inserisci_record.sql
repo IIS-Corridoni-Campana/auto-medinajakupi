1ee844c4bbc074b598c8adfb4488d7b1a39f2258
medinajakupi <medina.jakupi@corridoni-campana.edu.it>
2026-05-29 08:30:38 +0000

    MODIFICHE

--- inserisci_record.sql
+++ inserisci_record.sql
@@ -6,11 +6,19 @@ SELECT * FROM Costruttore;
 INSERT INTO Automobile (Targa, Modello, Tipo, Cilindrata, Alimentazione, Disponibilità, Anno_prod_modello, Nome_costruttore)
 VALUES ('TR009IE', 'Opel', 'Corsa'; 100, 'Diesel', 2, 2000, 'Marco');
 
+SELECT * FROM Automobile;
+
 INSERT INTO Patente (Numero,Data_rilascio,Data_scadenza,Tipologia)
 VALUES (23,'2017-03-21','2027-03-21','B');
+
+SELECT * FROM Patente;
 
 INSERT INTO Cliente (Codice_fiscale,Nome,Cognome,Data_nascita,Telefono,Numero_patente)
 VALUES ('RSSMRA85B15H501I','Medina','Jakupi','2002-07-16','03755106011',23);
 
+SELECT * FROM Cliente;
+
 INSERT INTO Noleggio (ID,Inizio_noleggio,Fine_noleggio,Costo,Targa_auto,Codice_fiscale_cliente)
 VALUES ('0SA','2026-05-01','2026-05-05',80,'TR009IE','RSSMRA85B15H501I');
+
+SELECT * FROM Noleggio;
