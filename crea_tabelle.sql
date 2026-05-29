e9fa0c5cf13ebdbb17f567963a67afde57d43f97
medinajakupi <medina.jakupi@corridoni-campana.edu.it>
2026-05-29 08:30:24 +0000

    MODIFICA

--- crea_tabelle.sql
+++ crea_tabelle.sql
@@ -1,1 +1,57 @@
--- USA QUESTO FILE PER CREARE LE TABELLE DEL DATABASE
+
+
+CREATE TABLE Costruttore
+(
+  Nome VARCHAR NOT NULL,
+  Città VARCHAR NOT NULL,
+  Nazione VARCHAR NOT NULL,
+  PRIMARY KEY (Nome)
+);
+
+CREATE TABLE Automobile
+(
+  Targa VARCHAR NOT NULL,
+  Modello VARCHAR NOT NULL,
+  Tipo VARCHAR NOT NULL,
+  Cilindrata INT NOT NULL,
+  Alimentazione VARCHAR NOT NULL,
+  Disponibilità INT NOT NULL,
+  Anno_prod_modello DATE NOT NULL,
+  Nome_costruttore VARCHAR NOT NULL,
+  PRIMARY KEY (Targa),
+  FOREIGN KEY (Nome_costruttore) REFERENCES Costruttore(Nome)
+);
+
+CREATE TABLE Patente
+(
+  Numero INT NOT NULL,
+  Data_rilascio DATE NOT NULL,
+  Data_scadenza DATE NOT NULL,
+  Tipologia VARCHAR NOT NULL,
+  PRIMARY KEY (Numero)
+);
+
+CREATE TABLE Cliente
+(
+  Codice_fiscale VARCHAR NOT NULL,
+  Nome VARCHAR NOT NULL,
+  Cognome VARCHAR NOT NULL,
+  Data_nascita DATE NOT NULL,
+  Telefono VARCHAR NOT NULL,
+  Numero_patente INT NOT NULL,
+  PRIMARY KEY (Codice_fiscale),
+  FOREIGN KEY (Numero_patente) REFERENCES Patente(Numero)
+);
+
+CREATE TABLE Noleggio
+(
+  ID VARCHAR NOT NULL,
+  Inizio_noleggio DATE NOT NULL,
+  Fine_noleggio DATE NOT NULL,
+  Costo FLOAT NOT NULL,
+  Targa_auto VARCHAR NOT NULL,
+  Codice_fiscale_cliente VARCHAR NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (Targa_auto) REFERENCES Automobile(Targa),
+  FOREIGN KEY (Codice_fiscale_cliente) REFERENCES Cliente(Codice_fiscale)
+);
--- inserisci_record.sql
+++ inserisci_record.sql
@@ -1,1 +1,16 @@
--- USA QUESTO FILE PER INSERIRE I DATI NEL DATABASE
+INSERT INTO Costruttore (Nome, Città, Nazione)
+VALUES ('Marco','Roma','Italia');
+
+SELECT * FROM Costruttore;
+
+INSERT INTO Automobile (Targa, Modello, Tipo, Cilindrata, Alimentazione, Disponibilità, Anno_prod_modello, Nome_costruttore)
+VALUES ('TR009IE', 'Opel', 'Corsa'; 100, 'Diesel', 2, 2000, 'Marco');
+
+INSERT INTO Patente (Numero,Data_rilascio,Data_scadenza,Tipologia)
+VALUES (23,'2017-03-21','2027-03-21','B');
+
+INSERT INTO Cliente (Codice_fiscale,Nome,Cognome,Data_nascita,Telefono,Numero_patente)
+VALUES ('RSSMRA85B15H501I','Medina','Jakupi','2002-07-16','03755106011',23);
+
+INSERT INTO Noleggio (ID,Inizio_noleggio,Fine_noleggio,Costo,Targa_auto,Codice_fiscale_cliente)
+VALUES ('0SA','2026-05-01','2026-05-05',80,'TR009IE','RSSMRA85B15H501I');
